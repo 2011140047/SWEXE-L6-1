6369567c10bd984f19893be4cbfb1dd7f1f5d047
Ubuntu <ubuntu@ip-172-31-45-45.ec2.internal>
2022-10-24 10:47:58 +0000

    modified

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,8 +27,18 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
-
+  
+  
+  
+  
+  #modified
+  config.assets.compile = true
+  
+  
+  
+  
+  
+  
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
